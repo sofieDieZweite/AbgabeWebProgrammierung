e9652784d40f3c679e967994614a230e6ed93d48
Sofie Wagner <sofie.wagner@gmx.net>
2024-04-04 16:15:19 +0200

    main page and eventpage

--- web/src/pages/event.js
+++ web/src/pages/event.js
@@ -1,3 +1,33 @@
 export default function Event(){
-    return <h1>Event</h1>
+    return (<>
+                <div>
+                    <h1 className="headline">Events</h1>
+                </div>
+                <div className="events">
+                    <div className="card" styles={{width: '10rem'}}>
+                        <img src="halloween.jpg" className="card-img-top" alt="kuchen"/>
+                        <div className="card-body">
+                            <h5 className="card-title">Halloween Backen</h5>
+                            <p className="card-text">Komm zu uns in die Backstube und lerne deine Torten passend zu Halloween zu dekorieren.</p>
+                            <a href="/quiz" className="btn-event" >weitere Informationen</a>
+                        </div>
+                    </div>
+                    <div className="card" styles={{width: '18rem'}}>
+                        <img src="probieren.jpg" className="card-img-top" alt="kuchen"/>
+                            <div className="card-body">
+                                <h5 className="card-title">Kuchen Bewertung</h5>
+                                <p className="card-text">Entscheiden Sie mit, welche neuen Kuchenkreation dauerhaft in unser Sortiment aufgenommen wird!</p>
+                                <a href="/quiz" className="btn-event" >weitere Informationen</a>
+                            </div>               
+                    </div>
+                    <div className="card" styles={{width: '18rem'}}>
+                        <img src="weihnachten.jpg" className="card-img-top" alt="kuchen"/>
+                            <div className="card-body">
+                                <h5 className="card-title">Weihnachtsbacken</h5>
+                                <p className="card-text">Erleben Sie mit uns die Adventszeit und Sie lernen nebenbei professionell Kekse zu dekorieren!</p>
+                                <a href="/quiz" className="btn-event" >weitere Informationen</a>
+                            </div>
+                    </div>
+                </div>
+            </>)
 }
--- web/src/pages/home.js
+++ web/src/pages/home.js
@@ -1,9 +1,37 @@
 export default function Home(){
     return (
     <>
-        <h1>Kuchen & Co</h1>
+        <div>
+            <h1 className="headline">Kuchen & Co</h1>
+        </div>
         <div className = "imageBox">
             <img className="img-fluid" src="kuchen.jpg" alt="Kuchen"></img>
+        </div>
+        <div className="grid-container">
+            <div className="textfeld-links">
+                <p>Wir von Kuchen & Co haben hier in Berlin unsere kleine Konditorei eröffnet um Sie mit Kuchen und Torten zu jedem Anlass zu verwöhnen. 
+                    </p> 
+                <p>Bei uns können Sie von uns zusammengestellte oder auf Wunsch erstellte Torten bestellen. </p>
+                    <p>Da Kuchen für uns aber nicht nur ein Essen, sondern auch ein Erlebnis ist, bieten wir verschiedene Veranstaltungen mit Kuchen, für die Sie unseren Laden direkt besuchen können.
+                    </p>
+                    <p> Wir freuen uns auf Sie!
+                </p>
+            </div>
+            <div className="cupcake">
+                <img className="img-fluid" src="kuchen2.jpg" alt="Kuchen"></img>
+            </div>
+        </div>
+        <div className="grid-container">
+            
+            <div className="cupcake">
+                <img className="img-fluid" src="backen.jpg" alt="Kuchen"></img>
+            </div>
+            <div className="textfeld-rechts">
+                <p>Mein Ehemann und ich backen schon seit wir klein sind. Die Liebe für Gebäck hat uns auch im Jahr 1985 bei unserer Konditorausbildung in Paris zusammengeführt.
+                </p>
+                <p>Jetzt haben wir uns endlich unseren Traum einer gemeinsamen Bäckerei erfüllt.
+                </p>
+            </div>
         </div>
     </>)
 
